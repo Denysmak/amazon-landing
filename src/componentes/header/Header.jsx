42c27d4e1de361ce1ea81bba403dbc819d2707e2
Denys Makene <denysmakene13@gmail.com>
2025-02-15 23:05:04 -0300

    adiciona circulos e linhas de entrega

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -6,26 +6,26 @@ import Logo from '../../assets/logo.png';
 
 
 
-export default function Header() {
-    
-    return (
-      <>
-        <nav className='header'>
-          <div className='logo'>
-            <img src={Logo} className='img-logo' alt='logo' />
-            <p>.com</p>
-          </div>
-  
+export default function Header({ naoexibir }) {
+  return (
+    <>
+      <nav className='header'>
+        <div className='logo'>
+          <img src={Logo} className='img-logo' alt='logo' />
+          <p>.com</p>
+        </div>
+        {!naoexibir ? (
           <div className='serach-box'>
             <input type="text" placeholder='Buscar Amazon.com' />
             <button><IoIosSearch fontSize={30} /></button>
           </div>
-  
-          <div className='infos'>
-            <p><FaInfoCircle style={{ marginRight: 8 }} />Ayuda y asistencia</p>
-            <p><FaCartShopping style={{ marginRight: 8 }} />Carrito</p>
-          </div>
-        </nav>
-      </>
-    );
-  }
+        ) : null}
+
+        <div className='infos'>
+          <p><FaInfoCircle style={{ marginRight: 8 }} />Ayuda y asistencia</p>
+          <p><FaCartShopping style={{ marginRight: 8 }} />Carrito</p>
+        </div>
+      </nav>
+    </>
+  );
+}
--- src/pages/Entrega/index.js
+++ src/pages/Entrega/index.js
@@ -4,8 +4,26 @@ import './style.css';
 export default function Entrega() {
   return (
     <>
-      <Header />
-        <h1>Teste</h1>
+      <Header naoexibir={true} />
+      <div className="circles-container">
+        <div className="containerCircleText">
+          <div className="circle" id="circle1">1</div>
+          <p>Selección</p>
+        </div>
+        <div className="line"></div>
+        <div className="containerCircleText">
+          <div className="circle" id="circle2">2</div>
+          <p>Información<br/> del pedido</p>
+        </div>
+        <div className="line linha2"></div>
+        <div className="containerCircleText">
+          <div className="circle" id="circle3">3</div>
+          <p>Pago</p>
+        </div>
+      </div>
+      <h1>testando</h1>
+      <h1>testando</h1>
+      <h1>testando</h1>
     </>
-  )
+  );
 }
